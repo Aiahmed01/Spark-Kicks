e9ccd2da50efe255b348ccb995174ee53521f9d0
Ermiyas B <124329320+Ermi-B@users.noreply.github.com>
2023-08-09 20:21:16 -0500

    added config folder

---
+++ server/config/seeds.js
@@ -0,0 +1,47 @@
+const db = require("./connection");
+const { User, Product, Category } = require("../models");
+
+db.once("open", async () => {
+  await Category.deleteMany();
+  await Product.deleteMany();
+  await User.deleteMany();
+
+  const categories = await Category.insertMany([
+    { name: "Food" },
+    { name: "Household Supplies" },
+    { name: "Electronics" },
+    { name: "Books" },
+    { name: "Toys" },
+  ]);
+  console.log('categories seeded');
+
+  const products = await Product.insertMany([
+    {
+      name: "Tin of Cookies",
+      description:
+        "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+      image: "cookie-tin.jpg",
+      category: categories[0]._id,
+      price: 2.99,
+      quantity: 500,
+    },
+  ]);
+  console.log('products seeded');
+
+  await User.create({
+    firstName: 'Pamela',
+    lastName: 'Washington',
+    email: 'pamela@testmail.com',
+    password: 'password12345',
+    orders: [
+      {
+        products: [products[0]._id, products[0]._id, products[1]._id]
+      }
+    ]
+  });
+
+
+  console.log('users seeded');
+
+  process.exit();
+});
